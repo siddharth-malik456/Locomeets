f8529b23584e4146dfdc1c48a399b7191447985e
nayan <nayanansh@gmail.com>
2024-02-23 01:55:50 +0530

    bug fix

--- client/src/components/CardContainer.jsx
+++ client/src/components/CardContainer.jsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
 import Card from "./Card";
 
-
 function CardContainer({ allServices }) {
   const [productID, setProductID] = useState(null);
-
   return (
     <div className="flex flex-wrap gap-x-8 pl-20 pr-20">
       {allServices &&
