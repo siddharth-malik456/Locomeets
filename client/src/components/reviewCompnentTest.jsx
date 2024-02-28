06d3a205828e8010c5b35c54154b2e1de3e49f92
nayan <nayanansh@gmail.com>
2024-02-28 19:01:55 +0530

    fixed Backend Bug
    fixed bugs caused after merge

--- client/src/components/reviewCompnentTest.jsx
+++ client/src/components/reviewCompnentTest.jsx
@@ -67,18 +67,22 @@ const ReviewCompnentTest = () => {
         return;
       }
       const r = (await axios.get("http://localhost:3000/review/getuser/" + uid))
-        .data[0];
+        .data;
       console.log("HERE IN SINGE REVIEW");
-      setCurrentRevice({
-        userFirstName: r.user.firstName,
-        userLastName: r.user.lastName,
-        rating: r.rating,
-        heading: r.heading,
-        description: r.description,
-        images: r.images,
-        profilePicture: "",
-        nationality: r.user.nationality,
-        date: r.dateOfReview,
+      r.map((re) => {
+        if (re.service == params.id) {
+          setCurrentRevice({
+            userFirstName: r.user.firstName,
+            userLastName: r.user.lastName,
+            rating: r.rating,
+            heading: r.heading,
+            description: r.description,
+            images: r.images,
+            profilePicture: "",
+            nationality: r.user.nationality,
+            date: r.dateOfReview,
+          });
+        }
       });
     };
     d();
@@ -184,7 +188,7 @@ const ReviewCompnentTest = () => {
         </div>
       ) : (
         <div className="flex justify-center mt-4">
-          <Button className="" onClick={open}>
+          <Button className=" m-8" onClick={open}>
             Write a Review
           </Button>
         </div>
--- server/models/review.model.js
+++ server/models/review.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const reviewSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "Users",
+    ref: "users",
     required: true,
   },
   service: {
